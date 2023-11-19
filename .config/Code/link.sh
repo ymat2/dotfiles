ab470ee3d99d450b4ff75569830b7b0e5732e0a4
ymat2 <yuki.matsuda.r7@dc.tohoku.ac.jp>
2023-11-19 18:37:17 +0900

    :arrow_up: Update install.py & Add R config files

--- .config/Code/link.sh
+++
@@ -1,7 +0,0 @@
-if [ $(uname) == "Darwin" ]; then
-  ln -sf ~/dotfiles/.config/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings.json
-  ln -sf ~/dotfiles/.config/Code/User/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-fi
-if [[ "$(uname -r)" == *microsoft* ]]; then
-  ln -sf ~/dotfiles/.config/Code/User/settings.json ~/.vscode-server/data/Machine/settings.json
-fi
--- install.py
+++ install.py
@@ -1,38 +1,61 @@
+"""
+Executing this script generate ~/install.sh.
+Check the content of install.sh and execute it.
+"""
+
 import os
-import subprocess as sp
 
 ignore = [".DS_Store", ".git", ".gitignore", "install.py"]
 home = os.path.expanduser("~")
 here = os.path.dirname(__file__)
 
-for root, dirs, files in os.walk(here):
-	for d in dirs:
-		src = os.path.relpath(os.path.join(root, d), here)
-		dst = os.path.join(home, src)
+log_and_cmd = []
 
-		if os.path.exists(dst):
-			print(d + ": Directory exists")
-		elif os.path.lexists(dst):
-			print(d + ": Broken link")
+def _ln_s(_target, _link):
+		if os.path.islink(_link):
+			log_and_cmd.append(f"# Warning: {_link}: Already linked")
+		elif os.path.exists(_link):
+			log_and_cmd.append(f"# {_link}: File exists")
 		else:
-			cmd = ["mkdir", dst]
-			print(" ".join(cmd))
-			sp.call(cmd)
+			log_and_cmd.append(f"ln -s {_target} {_link}")
 
 for root, dirs, files in os.walk(here):
 	for f in files:
-		src_abs = os.path.join(root, f)
-		src_rel = os.path.relpath(src_abs, here)
-		dst = os.path.join(home, src_rel)
+		target_full_path = os.path.join(root, f)
+		target_rel_dir = os.path.relpath(root, here)
+		target_top_dir = target_rel_dir.split(os.path.sep)[0]
+		link_dir = os.path.join(home, target_rel_dir)
+		link_full_path = link_dir + "/" + f
 
-		if f in ignore:
-			print(f + ": Ignored")
-			continue
-		if os.path.exists(dst):
-			print(f + ": File exists")
-		elif os.path.lexists(dst):
-			print(f + ": Broken link")
+		if f in ignore or target_top_dir in ignore:
+			log_and_cmd.append(f"# Ignore: {target_full_path}")
+
+		elif not os.path.exists(link_dir):
+			log_and_cmd.append(f"# {link_dir} does not exist.")
+			log_and_cmd.append(f"mkdir -p {link_dir}")
+			_ln_s(target_full_path, link_full_path)
+
 		else:
-			cmd = ["ln", "-s", src_abs, dst]
-			print(" ".join(cmd))
-			sp.call(cmd)
+			_ln_s(target_full_path, link_full_path)
+
+
+# Platform specific symlink
+if os.uname().sysname == 'Darwin':
+	for f in ["keybindings.json", "settings.json"]:
+		target_full_path = home+"/dotfiles/.config/Code/User/"+f
+		link_full_path = home+"/Library/Application\ Support/Code/User/"+f
+		_ln_s(target_full_path, link_full_path)
+
+elif "microsoft" in os.uname().release:
+	for f in ["keybindings.json", "settings.json"]:
+		target_full_path = home+"/dotfiles/.config/Code/User/"+f
+		link_full_path = "/mnt/c/Users/kokep/AppData/Roaming/Code/User/"+f
+		_ln_s(target_full_path, link_full_path)
+
+
+# Generate installer
+install_sh = os.path.join(home, "install.sh")
+with open(install_sh, "w") as f:
+	f.write("#!/bin/sh\n\n")
+	for line in log_and_cmd:
+		f.write(str(line)+"\n")
